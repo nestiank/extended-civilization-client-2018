bc95ff6564bb669a5ed38c9357a5c30e87b112b1
redsuncore <yjh4645@naver.com>
2018-01-11 20:34:14 +0900

    "우리민족 참역사 프로젝트"

---
+++ Assets/Scripts/CIVGameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CivPresenter;
+using CivModel;
+
+public class CIVGameManager : MonoBehaviour {
+
+    private static GameObject gameManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(this);
+        //System.Diagnostics.Debug.Assert(gameObject == null);
+		if(gameManager == null)
+        {
+            gameManager = this.gameObject;
+        }
+        else
+        {
+            Destroy(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
