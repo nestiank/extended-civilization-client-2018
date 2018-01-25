bbb94d3e06d75248af83a1f5abd46da26aec958b
16홍선우 <hsu97cow@naver.com>
2018-01-26 01:08:01 +0900

    DepPrefab modified by tiny bit

--- Assets/Scripts/PrefabScript/DepPrefab.cs
+++ Assets/Scripts/PrefabScript/DepPrefab.cs
@@ -1,16 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using CivModel;
+using CivModel.Common;
+using CivPresenter;
 
-public class DepPrefab : MonoBehaviour {
+public class DepPrefab : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Text unitName;
+    private Image unitPrt;
+    // Use this for initialization
+
+    void Awake()
+    {
+        Debug.Log("call DepPre");
+        unitName = gameObject.GetComponentsInChildren<Text>()[2];
+        unitName.text = "초기";
+
+        unitPrt = gameObject.GetComponentsInChildren<Image>()[0];
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public GameObject MakeItem(IProductionFactory fact)
+    {
+        string nameofFactory = ProductionFactoryTraits.GetFactoryName(fact);
+        unitPrt.sprite = Resources.Load<Sprite>("Unit_portrait/" + nameofFactory);
+        unitName.text = nameofFactory;
+        return this.gameObject;
+    }
+
+    public GameObject MakeItem()
+    {
+        if (unitName == null)
+        {
+            Debug.Log("Noname");
+        }
+        unitName.text = "배치 가능 유닛 없음";
+        unitName.fontSize = 10;
+        return this.gameObject;
+    }
 }
+
