fadf903aa0acccdd4cbf8b57bf625ae7541326e6
knuppe22 <knuppe22@gmail.com>
2018-02-08 22:08:04 +0900

    Make UIs & Import dll

--- Assets/Scripts/Hexmatrix.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class HexMatrix
-{
-    public const float outerRadius = 1.8f;
-    public const float innerRadius = outerRadius * 0.866025404f;
-}
