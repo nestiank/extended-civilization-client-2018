ad2c847329eb2acc7db65e179233e587ec990424
16홍선우 <hsu97cow@naver.com>
2018-02-11 16:51:49 +0900

    Production Selection UI (Management UI) enabled & Skill set UI enabled (Map UI)

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,24 +8,46 @@ public class UIManager : MonoBehaviour {
     public GameObject ManagementUI;
     public GameObject QuestUI;
 
-    public void MapUIActive()
+    public GameObject SkillSet;                   //** Map UI     ->      Does it need to be in MapUI.cs?
+
+    public GameObject UnitSelUI;                  //** Production Selection in Management UI    ->      Do they need to be in ManagementUI.cs?
+    public GameObject BuildingSelUI;
+//    public GameObject Tier1, Tier2, Tier3, Tier4;                 // for tab under tab
+//    public GameObject CIty, CityBuilding, NormalBuilding;
+
+    public void MapUIActive()               // Map UI tab
     {
         MapUI.SetActive(true);
         ManagementUI.SetActive(false);
         QuestUI.SetActive(false);
     }
-
-    public void ManagementUIActive()
+    public void ManagementUIActive()        // Management UI tab
     {
         ManagementUI.SetActive(true);
         MapUI.SetActive(false);
         QuestUI.SetActive(false);
     }
-
-    public void QuestUIActive()
+    public void QuestUIActive()             // Quest UI tab
     {
         QuestUI.SetActive(true);
         MapUI.SetActive(false);
         ManagementUI.SetActive(false);
+    }
+
+
+    public void SkillSetActive()               //** Map UI
+    {
+        SkillSet.SetActive(!SkillSet.activeSelf);
+    }
+
+    public void UnitSelUIActive()              //** Production Selection in Management UI
+    {
+        UnitSelUI.SetActive(true);
+        BuildingSelUI.SetActive(false);
+    }
+    public void BuildingSelUIActive()
+    {
+        UnitSelUI.SetActive(false);
+        BuildingSelUI.SetActive(true);
     }
 }
