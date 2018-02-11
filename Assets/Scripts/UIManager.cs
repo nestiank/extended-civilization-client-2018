7d6bae5fde19daee85bb0cbb99db857672594be2
16홍선우 <hsu97cow@naver.com>
2018-02-11 19:03:08 +0900

    All tabs enabled

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,50 +4,115 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour {
 
+    //// Resource bar UI ////
     public GameObject MapUI;
     public GameObject ManagementUI;
     public GameObject QuestUI;
 
-    public GameObject SkillSet;                   //** Map UI     ->      Does it need to be in MapUI.cs?
+    public GameObject SpecialSpec;
 
-    public GameObject UnitSelUI;                  //** Production Selection in Management UI    ->      Do they need to be in ManagementUI.cs?
-    public GameObject BuildingSelUI;
-//    public GameObject Tier1, Tier2, Tier3, Tier4;                 // for tab under tab
-//    public GameObject CIty, CityBuilding, NormalBuilding;
+    //// Map UI ////
+    public GameObject SkillSet;
 
-    public void MapUIActive()               // Map UI tab
+    //// Management UI (Production Selection) ////
+    public GameObject UnitSelTab;
+    public GameObject BuildingSelTab;
+    public GameObject EpicTab, HighTab, IntermediateTab, LowTab;    // Unit production
+    public GameObject CityTab, CityBuildingTab, NormalBuildingTab;  // Building production
+
+
+    //// Resource bar UI ////
+    public void MapUIActive()                   // Map UI tab
     {
         MapUI.SetActive(true);
         ManagementUI.SetActive(false);
         QuestUI.SetActive(false);
     }
-    public void ManagementUIActive()        // Management UI tab
+    public void ManagementUIActive()            // Management UI tab
     {
         ManagementUI.SetActive(true);
         MapUI.SetActive(false);
         QuestUI.SetActive(false);
     }
-    public void QuestUIActive()             // Quest UI tab
+    public void QuestUIActive()                 // Quest UI tab
     {
         QuestUI.SetActive(true);
         MapUI.SetActive(false);
         ManagementUI.SetActive(false);
     }
 
+    public void SpecialMouseOver()
+    {
+        SpecialSpec.SetActive(true);
+    }
+    public void SpecialMouseExit()
+    {
+        SpecialSpec.SetActive(false);
+    }
 
-    public void SkillSetActive()               //** Map UI
+    //// Map UI ////
+    public void SkillSetActive()
     {
         SkillSet.SetActive(!SkillSet.activeSelf);
     }
 
-    public void UnitSelUIActive()              //** Production Selection in Management UI
+    //// Management UI (Production Selection) ////
+    public void UnitSelTabActive()
     {
-        UnitSelUI.SetActive(true);
-        BuildingSelUI.SetActive(false);
+        UnitSelTab.SetActive(true);
+        BuildingSelTab.SetActive(false);
     }
-    public void BuildingSelUIActive()
+    public void BuildingSelTabActive()
     {
-        UnitSelUI.SetActive(false);
-        BuildingSelUI.SetActive(true);
+        UnitSelTab.SetActive(false);
+        BuildingSelTab.SetActive(true);
+    }
+    
+    public void EpicTabActive()                 // Unit production
+    {
+        EpicTab.SetActive(true);
+        HighTab.SetActive(false);
+        IntermediateTab.SetActive(false);
+        LowTab.SetActive(false);
+    }
+    public void HighTabActive()
+    {
+        EpicTab.SetActive(false);
+        HighTab.SetActive(true);
+        IntermediateTab.SetActive(false);
+        LowTab.SetActive(false);
+    }
+    public void IntermediateTabActive()
+    {
+        EpicTab.SetActive(false);
+        HighTab.SetActive(false);
+        IntermediateTab.SetActive(true);
+        LowTab.SetActive(false);
+    }
+    public void LowTabActive()
+    {
+        EpicTab.SetActive(false);
+        HighTab.SetActive(false);
+        IntermediateTab.SetActive(false);
+        LowTab.SetActive(true);
+    }
+    
+    public void CityTabActive()                 // Building production
+    {
+        CityTab.SetActive(true);
+        CityBuildingTab.SetActive(false);
+        NormalBuildingTab.SetActive(false);
+    }
+    public void CityBuildingTabActive()
+    {
+        CityTab.SetActive(false);
+        CityBuildingTab.SetActive(true);
+        NormalBuildingTab.SetActive(false);
+    }
+    public void NormalBuildingTabActive()
+    {
+        CityTab.SetActive(false);
+        CityBuildingTab.SetActive(false);
+        NormalBuildingTab.SetActive(true);
     }
 }
