93bd7d8981b698c3681168818b19315fe78f378e
16홍선우 <hsu97cow@naver.com>
2018-02-11 12:59:39 +0900

    UI tabs enabled

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public GameObject MapUI;
+    public GameObject ManagementUI;
+    public GameObject QuestUI;
+
+    public void MapUIActive()
+    {
+        MapUI.SetActive(true);
+        ManagementUI.SetActive(false);
+        QuestUI.SetActive(false);
+    }
+
+    public void ManagementUIActive()
+    {
+        ManagementUI.SetActive(true);
+        MapUI.SetActive(false);
+        QuestUI.SetActive(false);
+    }
+
+    public void QuestUIActive()
+    {
+        QuestUI.SetActive(true);
+        MapUI.SetActive(false);
+        ManagementUI.SetActive(false);
+    }
+}
