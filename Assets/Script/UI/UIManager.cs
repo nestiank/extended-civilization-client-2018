caef6b5ba561322fa659cbd5e33949223fd46c28
YangHun <pitahano@gmail.com>
2018-07-05 17:22:58 +0900

    UIManager Edit

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -10,8 +10,10 @@ public class UIManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        mapUI = GameObject.Find("MapUI");
-    }
+        GameObject map = GameObject.Find("MapUI");
+		GameObject management = GameObject.Find("ManagementUI");
+		GameObject quest = GameObject.Find("QuestUI");
+	}
 
     // Update is called once per frame
     void Update()
@@ -24,8 +26,10 @@ public class UIManager : MonoBehaviour
         if (go.activeSelf == false)
         {
             go.SetActive(true);
-            if (go != mapUI) mapUI.SetActive(false);
-        }
+			if (go != map) map.SetActive(false);
+			if (go != management) management.SetActive(false);
+			if (go != quest) quest.SetActive(false);
+		}
         else
         {
             go.SetActive(false);
