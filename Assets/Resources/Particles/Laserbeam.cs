4d8b395aabddef4dff0b7e91208896ce4307b951
CHAEHOSHIN-PC\ChaeHo <gshin3@live.com>
2018-09-13 02:30:27 +0900

    Cartoon FX/War Fx/Unit Particles/Attack Animation/Sound

---
+++ Assets/Resources/Particles/Laserbeam.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laserbeam : MonoBehaviour
+{
+    public GameObject StartParticle;
+    public GameObject EndParticle;
+    public GameObject lineParticle;
+
+    public Vector3 Target;
+    public float shootTime;
+
+    float distance;
+    Vector3 Direction;
+    Vector3 HitPoint;
+    LineRenderer Line;
+    GameObject StartObj;
+    GameObject EndObj;
+    GameObject LineObj;
+
+    public void Fire(Vector3 Target, float shootTime)
+    {
+        StartObj = Instantiate(StartParticle, transform);
+        StartObj.transform.localPosition = Vector3.zero;
+        EndObj = Instantiate(EndParticle, transform);
+        EndObj.SetActive(false);
+        LineObj = Instantiate(lineParticle, transform);
+        Line = LineObj.GetComponent<LineRenderer>();
+        this.Target = Target;
+        this.shootTime = shootTime;
+        EndObj.transform.position = Target;
+        distance = (Target - transform.position).magnitude;
+        Direction = (Target - transform.position).normalized;
+        StartCoroutine(_fire());
+    }
+
+    IEnumerator _fire()
+    {
+        float timer = 0;
+        Vector3[] pos = new Vector3[2];
+        pos[0] = transform.position;
+        HitPoint = transform.position;
+        while (timer < 5f)
+        {
+            if (Vector3.Distance(Target, HitPoint) > 0.01)
+                HitPoint = Vector3.MoveTowards(HitPoint, Target, distance / shootTime * Time.deltaTime);
+            else
+                break;
+            pos[1] = HitPoint;
+            Line.SetPositions(pos);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        EndObj.SetActive(true);
+        timer = 0;
+        while(timer < 1f)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Resources/Particles/Projectile.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public GameObject impactParticle;
+    public GameObject projectileParticle;
+    public GameObject muzzleParticle;
+    //public GameObject[] trailParticles;
+    [HideInInspector]
+    public Vector3 impactNormal; //Used to rotate impactparticle.
+
+    private GameObject impactObj;
+    private GameObject projectileObj;
+    private GameObject muzzleObj;
+    //private GameObject[] trailObjs;
+
+    public bool hasFired = false;
+    public bool hasCollided = false;
+
+    public void StartFire()
+    {
+        hasCollided = false;
+        projectileObj = Instantiate(projectileParticle, transform.position, transform.rotation) as GameObject;
+        projectileObj.transform.parent = transform;
+        if (muzzleParticle)
+        {
+            muzzleObj = Instantiate(muzzleParticle, transform.position, transform.rotation) as GameObject;
+            muzzleObj.transform.rotation = transform.rotation * Quaternion.Euler(180, 0, 0);
+            Destroy(muzzleObj, 1.5f); // Lifetime of muzzle effect.
+        }
+        hasFired = true;
+    }
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if (!hasCollided)
+        {
+            hasCollided = true;
+            impactObj = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, impactNormal)) as GameObject;
+
+            /*
+            foreach (GameObject trail in trailParticles)
+            {
+                GameObject curTrail = transform.Find(projectileParticle.name + "/" + trail.name).gameObject;
+                curTrail.transform.parent = null;
+                Destroy(curTrail, 3f);
+            }*/
+            Destroy(projectileObj, 3f);
+            Destroy(impactObj, 5f);
+            gameObject.SetActive(false);
+            hasFired = false;
+            ParticleSystem[] trails = GetComponentsInChildren<ParticleSystem>();
+            //Component at [0] is that of the parent i.e. this object (if there is any)
+            for (int i = 1; i < trails.Length; i++)
+            {
+                ParticleSystem trail = trails[i];
+
+                if (trail.gameObject.name.Contains("Trail"))
+                {
+                    trail.transform.SetParent(null);
+                    Destroy(trail.gameObject, 2f);
+                }
+            }
+        }
+    }
+}
